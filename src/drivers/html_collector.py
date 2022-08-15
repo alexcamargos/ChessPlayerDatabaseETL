0d8120984860ba2d364aeac1a45cef998f806e70
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-08-15 17:12:28 -0300

    feat: implement of HtmlCollector

---
+++ src/drivers/html_collector.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+#  ------------------------------------------------------------------------------
+#  Name: html_collector.py
+#  Version: 0.0.1
+#  Summary: Chess Player Database
+#           A complete implementation of ETL process.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+#  ------------------------------------------------------------------------------
+
+from bs4 import BeautifulSoup as bs
+
+
+class HtmlCollector:
+
+    @classmethod
+    def collect_information(cls, html: str):
+        """Collect information from the html."""
+
+        soup = bs(html, 'html.parser')
+        chess_players_table = soup.find("table", {'border': "0",
+                                                  'cellpadding': "2",
+                                                  'cellspacing': "0",
+                                                  'width': "100%"})
+        chess_players_links = chess_players_table.find_all('a')
+
+        chess_players_information = []
+        for chess_players in chess_players_links:
+            if not '.mp3' in chess_players.get('href'):
+                href = f"https://www.chessgames.com/{chess_players.get('href')}"
+                player_name = chess_players.contents[0].string.strip()
+                chess_players_information.append({'href': href,
+                                                  'player': player_name})
+            else:
+                continue
+
+        return chess_players_information
---
+++ src/drivers/html_collector_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+#  ------------------------------------------------------------------------------
+#  Name: html_collector_test.py
+#  Version: 0.0.1
+#  Summary: Chess Player Database
+#           A complete implementation of ETL process.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+#  ------------------------------------------------------------------------------
+
+
+from .html_collector import HtmlCollector
+from .mocks.http_requester_mock import mock_request_from_page
+
+
+def test_collect_information():
+    http_request_response = mock_request_from_page()
+
+    html_collector = HtmlCollector()
+    collect_information = html_collector.collect_information(
+        http_request_response['html'])
+
+    assert isinstance(collect_information, list)
+    assert isinstance(collect_information[0], dict)
+
+    assert 'href' in collect_information[0]
+    assert 'player' in collect_information[0]
