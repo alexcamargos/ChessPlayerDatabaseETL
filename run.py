cafda20da5670308ee0d8aa7146013455f4e511a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-08-25 06:25:00 -0300

    feat: add progress bar for execution process using tqdm module

--- run.py
+++ run.py
@@ -13,16 +13,37 @@
 #  License: MIT
 #  ------------------------------------------------------------------------------
 
-from time import sleep
+import time
+
+from tqdm import tqdm
+
 from src.main.main_pipeline import MainPipeline
 
 if __name__ == "__main__":
-    # A list with A-Z.
+    # A string list with all the letters of the alphabet A-Z.
     alphabet = [chr(i) for i in range(65, 91)]
 
-    for letter in alphabet:
+    print('Chess Player Database Pipeline Started!')
+    print('This process will take a while...\n\n')
+
+    # Get the start time.
+    start_time = time.time()
+
+    for letter in tqdm(alphabet,
+                       total=len(alphabet),
+                       unit_scale=True,
+                       colour='green'):
         URL = f'https://www.chessgames.com/directory/{letter}.html'
-        print(f'Processing {URL}')
         pipeline = MainPipeline(url=URL)
         pipeline.run()
-        sleep(10)  # Sleep 10 seconds to avoid too many requests.
+        time.sleep(10)  # Sleep 10 seconds to avoid too many requests.
+
+    # Get the end time.
+    end_time = time.time()
+
+    # Calculate the elapsed time.
+    elapsed_time = end_time - start_time
+    gmtime = time.gmtime(elapsed_time)
+
+    print('\n\nChess Player Database Pipeline Finished!')
+    print(f'Execution time: {time.strftime("%H:%M:%S", gmtime)}')
