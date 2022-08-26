b1518620a7f3db68f20b0a6403fcad1f5c30e622
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-08-26 06:09:41 -0300

    refactor: remove src/infrastructure/mocks/row_test_data.py is redundant code

--- src/infrastructure/database_repository_test.py
+++ src/infrastructure/database_repository_test.py
@@ -13,22 +13,24 @@
 #  License: MIT
 #  ------------------------------------------------------------------------------
 
+from src.stages.contracts.mocks.transform_contract import \
+    transform_contract_mock
+
 from .database_connection_handler import DatabaseConnectionHandler
 from .database_repository import DatabaseRepository
-from .mocks.row_test_data import row_test_data
 
 database = DatabaseRepository(connection_handler=DatabaseConnectionHandler())
 
 
 def test_insert_chess_player():
 
-    for row_data in row_test_data:
+    for row_data in transform_contract_mock.load_information:
         database.insert_chess_player(row_data)
 
 
 def test_list_all_chess_players():
 
     all_chess_players = database.list_all_chess_players()
-    assert len(all_chess_players) == len(row_test_data)
+    assert len(all_chess_players) == len(transform_contract_mock.load_information)
 
     database.database.drop_all_tables(with_all_data=True)
